cd8dcbea6ac82b6f50c98080235b39bdf719b907
Lucas Menescal <lucasmenescalpontes@gmail.com>
2023-08-09 16:47:03 -0300

    Adicionado novos endpoints

--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -11,25 +11,6 @@ class EnderecoController extends Controller
     /**
      * Busca na API ViaCepApi um Endereço.
      */
-    public function salvarEnderecoViaApi($cep)
-    {
-        $endereco = Endereco::where('cep', $cep)->first();
-
-        if (!$endereco) {
-            $response = Http::get("https://viacep.com.br/ws/{$cep}/json/");
-
-            if ($response->ok()) {
-                $data = $response->json();
-                return response()->json($data);
-            }
-        }
-
-        return response()->json($endereco);
-    }
-
-    /**
-     * Busca na API ViaCepApi um Endereço.
-     */
     public function buscar($cep)
     {
         $endereco = Endereco::where('cep', $cep)->first();
@@ -46,8 +27,6 @@ class EnderecoController extends Controller
         return response()->json($endereco);
     }
 
-
-
     /**
      * Lista todos os Endereços salvos no banco de dados.
      */
@@ -58,23 +37,77 @@ class EnderecoController extends Controller
     }
 
     /**
-     * Salva no Banco um Endereço
+     * Salva no Banco um Endereço enviado como request
      */
     public function save(Request $request)
     {
-        $endereco = Endereco::create([
-            'cep' => $request->cep,
-            'logradouro' => $request->logradouro,
-            'complemento' => $request->complemento,
-            'bairro' => $request->bairro,
-            'localidade' => $request->cidade,
-            'uf' => $request->estado,
-            'ibge' => $request->ibge,
-            'gia' => $request->gia,
-            'ddd' => $request->ddd,
-            'siafi' => $request->siafi,
+        try {
+            if ($request) {
+                $endereco = Endereco::create([
+                    'cep' => $request->cep,
+                    'logradouro' => $request->logradouro,
+                    'complemento' => $request->complemento,
+                    'bairro' => $request->bairro,
+                    'localidade' => $request->localidade,
+                    'uf' => $request->uf,
+                    'ibge' => $request->ibge,
+                    'gia' => $request->gia,
+                    'ddd' => $request->ddd,
+                    'siafi' => $request->siafi
+                ]);
+            }
+            return response()->json($endereco, 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Erro ao salvar. Endereço ja existe.'], 500);
+        }
+    }
+
+    /**
+     * Deleta um Endereço do banco.
+     */
+    public function delete($cep)
+    {
+        $cep = $this->formatarCep($cep);
+        $endereco = Endereco::where('cep', $cep)->first();
+
+        if (!$endereco) {
+            return response()->json(['message' => 'Endereço não encontrado'], 404);
+        }
+
+        $endereco->delete();
+
+        return response()->json(['message' => 'Endereço deletado com sucesso'], 200);
+    }
+
+    private function formatarCep($cep)
+    {
+        return substr($cep, 0, 5) . '-' . substr($cep, 5, 3);
+    }
+
+    /**
+     * Edita um Endereço do banco.
+     */
+    public function update(Request $request, $cep)
+    {
+        $cep = $this->formatarCep($cep);
+        $endereco = Endereco::where('cep', $cep)->first();
+
+        if (!$endereco) {
+            return response()->json(['message' => 'Endereço não encontrado'], 404);
+        }
+
+        $endereco->update([
+            'logradouro' => $request->input('logradouro', $endereco->logradouro),
+            'complemento' => $request->input('complemento', $endereco->complemento),
+            'bairro' => $request->input('bairro', $endereco->bairro),
+            'localidade' => $request->input('localidade', $endereco->localidade),
+            'uf' => $request->input('uf', $endereco->uf),
+            'ibge' => $request->input('ibge', $endereco->ibge),
+            'gia' => $request->input('gia', $endereco->gia),
+            'ddd' => $request->input('ddd', $endereco->ddd),
+            'siafi' => $request->input('siafi', $endereco->siafi)
         ]);
 
-        return response()->json($endereco, 201);
+        return response()->json(['message' => 'Endereço atualizado com sucesso'], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnderecoController;
 
 // Route::get('enderecos', [EnderecoController::class, 'index'])->name('enderecos.index');
-Route::get('/enderecos/buscar/{cep}', [EnderecoController::class, 'buscarEnderecoViaApi'])->name('EnderecoController.buscarEnderecoViaApi');
+Route::get('/enderecos/buscar/{cep}', [EnderecoController::class, 'buscar'])->name('EnderecoController.buscar');
 Route::get('/enderecos/listarTudo', [EnderecoController::class, 'listarTudo'])->name('EnderecoController.listarTudo');
 Route::get('/enderecos/save', [EnderecoController::class, 'save'])->name('EnderecoController.save');
+Route::get('/enderecos/delete/{cep}', [EnderecoController::class, 'delete'])->name('EnderecoController.delete');
+Route::get('/enderecos/update/{cep}', [EnderecoController::class, 'update'])->name('EnderecoController.update');
 
 Route::get('/', function () {
     return view('welcome');
