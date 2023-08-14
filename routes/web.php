4d58283857ad507f9a826a3482803f64a6a9e71e
Lucas Menescal <lucasmenescalpontes@gmail.com>
2023-08-14 10:04:37 -0300

    Revert "Ajustes para recebimento de Requisições + Readme"
    This reverts commit bf1a9061ff10cdec6837bc0ae58a2edd4f8eda42.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\EnderecoController;
 // Route::get('enderecos', [EnderecoController::class, 'index'])->name('enderecos.index');
 Route::get('/enderecos/buscar/{cep}', [EnderecoController::class, 'buscar'])->name('EnderecoController.buscar');
 Route::get('/enderecos/listarTudo', [EnderecoController::class, 'listarTudo'])->name('EnderecoController.listarTudo');
-Route::delete('/enderecos/delete/{cep}', [EnderecoController::class, 'delete'])->name('EnderecoController.delete');
+Route::get('/enderecos/save', [EnderecoController::class, 'save'])->name('EnderecoController.save');
+Route::get('/enderecos/delete/{cep}', [EnderecoController::class, 'delete'])->name('EnderecoController.delete');
+Route::get('/enderecos/update/{cep}', [EnderecoController::class, 'update'])->name('EnderecoController.update');
 
-Route::post('/enderecos/save', [EnderecoController::class, 'save'])->name('EnderecoController.save');
-Route::put('/enderecos/update', [EnderecoController::class, 'update'])->name('EnderecoController.update');
+Route::get('/', function () {
+    return view('welcome');
+});
+
